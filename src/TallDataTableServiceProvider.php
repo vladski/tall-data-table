ad4de1e3d0f3f0c401cd3873290407d0459d842a
tanthammar <tinahammar@gmail.com>
2020-04-21 20:01:28 +0200

    moved serviceprovider

--- src/TallDataTableServiceProvider.php
+++ src/TallDataTableServiceProvider.php
@@ -17,12 +17,11 @@ class TallDataTableServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->commands([MakeTable::class]);
+            $this->publishes([__DIR__ . '/../config/tall-data-table.php' => config_path('tall-data-table.php')], 'config');
+            $this->publishes([__DIR__ . '/../resources/views' => resource_path('views/vendor/tall-data-table')], 'views');
         }
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'tall-data-table');
-
-        $this->publishes([__DIR__ . '/../config/tall-data-table.php' => config_path('tall-data-table.php')], 'tall-data-table');
-        $this->publishes([__DIR__ . '/../resources/views' => resource_path('views/vendor/tall-data-table')], 'tall-data-table');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'tall-data-table');
     }
 
     public function register()
